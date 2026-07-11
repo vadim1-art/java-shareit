d208a3c7fc60f7ed307bebb5e223370c34dcacff
vadim_gotovec <vadimgotovec1@gmail.com>
2026-07-11 19:29:54 +0300

    # исправление Build application - 3

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
