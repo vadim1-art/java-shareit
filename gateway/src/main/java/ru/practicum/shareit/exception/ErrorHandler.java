5a8f7b7c3092d022c8a0e752563c3f6ff1b2742c
vadim_gotovec <vadimgotovec1@gmail.com>
2026-07-22 19:19:20 +0300

    # исправление Build application

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -8,36 +8,46 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
+    // Перехватывает ошибки от @Valid
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        log.warn("Ошибка валидации @Valid: {}", e.getMessage());
+        String defaultMessage = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
+        return Map.of("error", defaultMessage != null ? defaultMessage : "Ошибка валидации");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(final ValidationException e) {
+        log.warn("Ошибка 400: {}", e.getMessage());
+        return Map.of("error", e.getMessage());
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNotFoundException(final NotFoundException e) {
+    public Map<String, String> handleNotFound(final NotFoundException e) {
         log.warn("Ошибка 404: {}", e.getMessage());
         return Map.of("error", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public Map<String, String> handleConflictException(final ConflictException e) {
+    public Map<String, String> handleConflict(final ConflictException e) {
         log.warn("Ошибка 409: {}", e.getMessage());
-        return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(final Exception e) {
-        log.warn("Ошибка 400: {}", e.getMessage());
         return Map.of("error", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleThrowable(final Throwable e) {
-        log.error("Непредвиденная ошибка: ", e);
-        return Map.of("error", "Внутренняя ошибка сервера");
+        log.error("Ошибка 500: ", e);
+        return Map.of("error", e.getMessage() != null ? e.getMessage() : "Внутренняя ошибка сервера");
     }
 }
